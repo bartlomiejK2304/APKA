19dd3f7cc2cd877a5a0a3c91c55b92d550468795
Bartłomiej <klajnbartlomiej@gmail.com>
2026-01-09 21:46:21 +0100

    Dodanie Klasy Osoba

---
+++ APKA/Osoba.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APKA
+{
+    public abstract class Osoba
+    {
+        public string ?Imie { get; set; }
+        public string ?Nazwisko { get; set; }
+
+        private string _pesel = string.Empty;
+
+        public string Pesel
+        {
+            get { return _pesel; }
+            set
+            {
+                if (value != null && value.Length == 11)
+                {
+                    _pesel = value;
+                }
+                else
+                {
+                    throw new ArgumentException("PESEL musi mieć dokładnie 11 znaków.");
+                }
+            }
+        }
+    }
+
+}
