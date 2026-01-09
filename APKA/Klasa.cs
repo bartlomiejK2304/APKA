520ab26f1689c8ae6c0a4e104712eb02260eb721
vilotek <kornelkori@onet.pl>
2026-01-09 22:03:05 +0100

    Dodanie klasy klasa

---
+++ APKA/Klasa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APKA
+{
+    public class Klasa
+    {
+        public string nazwaKlasy;
+        public List<Uczen> klasa = new List<Uczen>();
+    }
+}
